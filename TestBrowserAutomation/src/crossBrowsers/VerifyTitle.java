4d1af924f3d5a16d9982abf23d4b5959b8ccece6
sharadsamant12 <61339440+sharadsamant12@users.noreply.github.com>
2020-06-09 07:29:29 +0530

    Admin login, Font size, Font Type Font colour verification

--- TestBrowserAutomation/src/crossBrowsers/VerifyTitle.java
+++ TestBrowserAutomation/src/crossBrowsers/VerifyTitle.java
@@ -1,5 +1,6 @@
 package crossBrowsers;
 
+
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
@@ -11,8 +12,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.Color;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
@@ -27,7 +30,10 @@ public class VerifyTitle {
 	@Parameters("browser")
 	public void verifypageTitle(String browserName) throws Exception {
 		
+		
+		
 		if(browserName.equalsIgnoreCase("firefox")) {
+			//System.setProperty("webdriver.Firefox.drive ", "D:\\eclipse\\New folder\\geckodriver.exe");
 			driver =new FirefoxDriver();
 		}
 		
@@ -44,42 +50,95 @@ public class VerifyTitle {
 			//driver=new edge();
 		}
 		
-		
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(50000, TimeUnit.SECONDS);
-		//Thread.sleep(5000);
+		  driver.manage().window().maximize();
+		  driver.manage().timeouts().implicitlyWait(50000, TimeUnit.SECONDS);
+		  
+		  
+		 
+		//Thread.sleep(50000);
+		  //driver.get("https://webuser:w3bus3r@stage2.dayre.me/auth/login");
+		driver.get("http://webuser:w3bus3r@webapp-stage.dayre.me/auth/login");
+				
 		driver.get("http://webapp-stage.dayre.me/auth/login");
-		Thread.sleep(20000);
+		Thread.sleep(1000);
 		driver.manage().timeouts().implicitlyWait(500, TimeUnit.SECONDS);
 		
-		// Font size
-		String fsize = driver.findElement(By.xpath("//h1[contains(text(),'Welcome home')]")).getCssValue("font-size");
-		System.out.println("Font size: " +fsize);
 		
 				
-		// Font Colour
-		String color = driver.findElement(By.xpath("//h1[contains(text(),'Welcome home')]")).getCssValue("color");
-		System.out.println("Color: " +color);
-						
 		
-		// Font Background Color
-		String bColor = driver.findElement(By.xpath("//h1[contains(text(),'Welcome home')]")).getCssValue("Background-Color");
-		System.out.println("Background Color: " +bColor);
 		
+		  //Font size  
+		  //String expected1 = "51px";
+		  String fsize = driver.findElement(By.xpath("//h1[contains(text(),'Welcome home')]")).getCssValue("font-size"); 
+		  System.out.println("Font size: " +fsize);
+		  
+		  //Font Type
+		  String fontfamily = driver.findElement(By.xpath("//h1[contains(text(),'Welcome home')]")).getCssValue("font-family");
+		  System.out.println("Font size: " +fontfamily);
+		
+		
+	
+		 
+		 // Font Colour compare for "Welcome home"  
+		  
+		 String expected = "#333c42";
+		 String bColor = driver.findElement(By.xpath("//h1[contains(text(),'Welcome home')]")).getCssValue("color");
+		 //String bColor = driver.findElement(By.xpath("//h1[contains(text(),'Welcome home')]")).getCssValue("Background-Color");
+		 //System.out.println("Background Color: " +bColor);
+		 String hexColor = Color.fromString(bColor).asHex();
+		 System.out.println("Welcome Home text colour is: " + hexColor);
+		 String actual = hexColor;
+		 Assert.assertEquals(actual, expected);
+		 
 		
 		
 		//Login and Password
 		driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[2]/div[1]/form[1]/div[1]/input[1]")).sendKeys("sharad");
-		driver.manage().timeouts().implicitlyWait(1000, TimeUnit.SECONDS);
-		driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[2]/div[1]/form[1]/div[2]/input[1]")).sendKeys("43214321");
-		driver.manage().timeouts().implicitlyWait(1000, TimeUnit.SECONDS);
+									
+		driver.manage().timeouts().implicitlyWait(10000, TimeUnit.SECONDS);
+		//Thread.sleep(10000);
+		//driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[2]/div[1]/form[1]/div[2]/input[1]")).sendKeys("43214321");
+		
+		WebDriverWait wait = new WebDriverWait(driver,30);
+
+		WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[2]/div[1]/form[1]/div[2]/input[1]")));
+		element.sendKeys("43214321");	
+		
+		
+		//driver.findElement(By.xpath("//input[@name='login_p']")).sendKeys("43214321");
+		driver.manage().timeouts().implicitlyWait(10000, TimeUnit.SECONDS);
+		//Thread.sleep(10000);
 		
 		driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[2]/div[1]/form[1]/div[4]/button[1]")).click();
 		driver.manage().timeouts().implicitlyWait(500, TimeUnit.SECONDS);
 		
 		
+		//Add Test Post
+		/*
+		 * driver.findElement(By.xpath(
+		 * "//body/div[@id='__next']/section/div/div/div/div/div/div/div[2]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]"
+		 * )).click();
+		 * 
+		 * driver.manage().timeouts().implicitlyWait(2500, TimeUnit.SECONDS);
+		 * driver.findElement(By.
+		 * xpath("//textarea[@placeholder=\"What's on your mind, sharad?\"]")).
+		 * sendKeys("The European languages are members of the same family. Their separate existence is a myth. For science, music, sport, etc, Europe uses the same vocabulary.\r\n"
+		 * + "\r\n" +
+		 * "The languages only differ in their grammar, their pronunciation and their most common words. Everyone realizes why a new common language would be desirable: one could refuse to pay expensive translators.\r\n"
+		 * + "\r\n" +
+		 * "To achieve this, it would be necessary to have uniform grammar, pronunciation and more common words. If several languages coalesce, the grammar of the resulting language is more simple and regular than that of the individual languages.\r\n"
+		 * + "\r\n" +
+		 * "The new common language will be more simple and regular than the existing European languages. It will be as simple as Occidental; in fact, it will be Occidental.\r\n"
+		 * + "\r\n" +
+		 * "To an English person, it will seem like simplified English, as a skeptical Cambridge friend of mine told me what Occidental is. The European languages are members of the same family. Their separate existence is a myth. For science, music, sport, etc, Europe uses the same vocabulary. The languages only differ in their grammar, their pronunciation and their most common words. Everyone realizes why a new common language would be desirable: one could refuse to pay expensive translators. :) ;)"
+		 * ); driver.manage().timeouts().implicitlyWait(2500, TimeUnit.SECONDS);
+		 * driver.findElement(By.xpath("//button[contains(text(),'Post')]")).click();
+		 * driver.manage().timeouts().implicitlyWait(2500, TimeUnit.SECONDS);
+		 * driver.findElement(By.xpath("//button[contains(text(),'Refresh')]")).click();
+		 * driver.manage().timeouts().implicitlyWait(2500, TimeUnit.SECONDS);
+		 */
 		
-				
+		
 		//Add comment on Feed page
 				
 		WebElement Textarea =driver.findElement(By.tagName("textarea"));
@@ -176,7 +235,7 @@ public class VerifyTitle {
 				driver.findElement(By.xpath("//div[8]//div[1]//div[1]//div[2]//div[2]//div[1]//div[1]//div[2]//div[1]//a[1]//img[1]")).click();
 				driver.manage().timeouts().implicitlyWait(500, TimeUnit.SECONDS);
 				Thread.sleep(3000);
-				////driver.findElement(By.xpath("//div[@class='icon-delete']//img")).click();
+				
 				
 				
 				Set<String> allWindows1=driver.getWindowHandles();
@@ -188,17 +247,12 @@ public class VerifyTitle {
 							driver.switchTo().window(child);
 														
 							Actions act = new Actions(driver);
-							//act.moveToElement(driver.findElement(By.xpath("//body/div[@id='__next']/section/div/div/div/div/div/div/div/div/section/div/div[2]/div[1]"))).perform();
-							//act.moveToElement(driver.findElement(By.cssSelector("section.page-section.clearfix div.block-section.welcome-section.clearfix div.container div.row div.col-12 div.mid_content div.postdetail div.row.no-gutters div.col-md-4 section.post-container-commentbox div.day-right-content div:nth-child(2) div.profile-comment-list > div.commentlist-wrapper:nth-child(1)"))).perform();
+							
 							act.moveToElement(driver.findElement(By.xpath("//div[@id='__next']//section//div//div//div//div//div//div//div//div//section//div//div//div//div//div//div//div//p"))).perform();
 							
 							Thread.sleep(1500);
 							new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.cssSelector("section.page-section.clearfix div.block-section.welcome-section.clearfix div.container div.row div.col-12 div.mid_content div.postdetail div.row.no-gutters div.col-md-4 section.post-container-commentbox div.day-right-content div:nth-child(2) div.profile-comment-list div.commentlist-wrapper:nth-child(1) > div.icon-delete"))).click();
-							//new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='icon-delete']"))).click();
-							//driver.findElement(By.xpath("//body/div[@id='__next']/section/div/div/div/div/div/div/div/div/section/div/div[2]/div[1]/div[1]/div[2]")).click();
-							//driver.findElement(By.xpath("//div[@class='icon-delete']")).click();
-							//Thread.sleep(5000);
-							
+							Thread.sleep(1000);							
 							driver.findElement(By.xpath("//button[contains(text(),'Yes')]")).click();
 							Thread.sleep(3000);
 							
@@ -210,14 +264,53 @@ public class VerifyTitle {
 				driver.switchTo().window(parent);
 				
 				
+		//Left Menu Item click and open post like or unlike the post
 				
+				driver.findElement(By.xpath("//section//li[6]//a[1]")).click();
+				Thread.sleep(1500);
+				driver.findElement(By.xpath("//body/div[@id='__next']/section/div/div/div/div/div/div/div/div/div/div/div[1]/div[1]/div[1]/div[2]/div[1]")).click();
+				Thread.sleep(1500);
+				
+				Set<String> allWindows2=driver.getWindowHandles();
+				for(String child:allWindows2)
+				{
+					if(!parent.equalsIgnoreCase(child))
+					{
+						driver.switchTo().window(child);
+													
+						//Actions act = new Actions(driver);
+						
+						//act.moveToElement(driver.findElement(By.xpath("//body/div[@id='__next']/section/div/div/div/div/div/div/div/div/section/div/div[1]/div[1]/div[1]/div[1]/div[1]"))).click();
+						driver.findElement(By.xpath("//body/div[@id='__next']/section/div/div/div/div/div/div/div/div/section/div/div[1]/div[1]/div[1]/div[1]/div[1]")).click();
+						Thread.sleep(2500);
+												
+						driver.close();
+					}
+				}
+			
+			driver.switchTo().window(parent);
+			driver.navigate().refresh();
+			Thread.sleep(3500);
+				
+				
+				
+				
+		
+		//Top Menu Item
+				driver.findElement(By.xpath("//a[@id='navbardrop']")).click();
+				Thread.sleep(1500);
+				driver.findElement(By.xpath("//a[contains(text(),'Settings')]")).click();
+				Thread.sleep(3500);
+				driver.findElement(By.xpath("//a[@id='navbardrop']")).click();
+				Thread.sleep(1500);
+				driver.findElement(By.xpath("//a[contains(text(),'Log Out')]")).click();
 	
 				
 		
 		
 		
 		//System.out.println(driver.getTitle());
-		//driver.quit();
+		driver.quit();
 	}
 
 }
